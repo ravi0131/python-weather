0b3d3b8835ee81fda03bf76660625eb1288bd7d9
ravi0131 <ravi.kumar@stud.tu-darmstadt.de>
2023-10-02 20:47:59 +0200

    first commit

---
+++ weather.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv() # load API_KEY value from our env file
+
+def get_current_weather(city = "Mannheim"):
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv('API_KEY')}&q={city}&units=metric"
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+if __name__ == "__main__":
+    print("\n*** Get Current Weather Conditions ***\n")
+    city = input("\nPlease enter a city name: ")
+    #check for empty strings or strings with only spaces
+    if not bool(city.strip()):
+        city = "Frankfurt"
+    weather_data = get_current_weather(city)
+    print("\n")
+    pprint(weather_data)
